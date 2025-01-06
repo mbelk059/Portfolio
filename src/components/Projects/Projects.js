b4675fcde6d11db9665dcba48d12e207e441a8cf
mbelk059 <mbelk818@gmail.com>
2025-01-05 21:21:15 -0500

    projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
+import livesite from "../../Assets/Projects/livesite.png";
+import emotion from "../../Assets/image.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import ucollab from "../../Assets/Projects/ucollab.png";
+import bunnygame from "../../Assets/Projects/bunnygame.png";
+import object from "../../Assets/Projects/objectdetect.png";
+import insuwell from "../../Assets/Projects/insuwell.png";
 
 function Projects() {
   return (
@@ -34,45 +36,44 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={insuwell}
               isBlog={false}
               title="insuWell"
               description="An AI-driven wellness coach designed to improve insulin sensitivity and help manage blood sugar levels through personalized lifestyle optimization."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              ghLink="https://github.com/mbelk059/Hack-the-Hill-2-insuWell?tab=readme-ov-file"
+              demoLink="https://devpost.com/software/insuwell"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={object}
               isBlog={false}
               title="Real-Time Object Detector"
               description="A real-time object detection app built with a pre-trained COCO SSD model."
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              ghLink="https://github.com/mbelk059/ReactComputerVisionTemplate"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={ucollab}
               isBlog={false}
               title="uCollab"
               description="A chat web app similar to Discord where university students can collaborate, share resources, and connect effortlessly, fostering a vibrant online community."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              ghLink="https://github.com/mbelk059/UottaHack6-Challenge"
+              demoLink="https://devpost.com/software/ucollab-94cxz1"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={livesite}
               isBlog={false}
               title="uOttaHack 2025 Live Site"
               description="."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              ghLink="https://github.com/uOttaHack/2025-live-site"
+              demoLink="https://live.uottahack.ca/" 
             />
           </Col>
 
@@ -93,8 +94,7 @@ function Projects() {
               isBlog={false}
               title="The Hidden Farm Village"
               description="A cozy farming game built with Pygame."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              ghLink="https://github.com/mbelk059/The-Hidden-Farm-Village"
             />
           </Col>
 
@@ -115,18 +115,18 @@ function Projects() {
               isBlog={false}
               title="Travel With Us"
               description="."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              ghLink="https://github.com/mbelk059/Travel-With-Us-2"
+              demoLink="https://mbelk059.github.io/Travel-With-Us-2/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={bunnygame}
               isBlog={false}
               title="Bunny Game"
               description="A cute pixel RPG built with GameMaker."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              ghLink="https://github.com/mbelk059/Bunny-Game"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
